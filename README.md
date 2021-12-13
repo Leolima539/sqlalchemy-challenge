f212906c50cabb63056b6df1ef221bd1c3623418
Eleo <eleonorajmartin@Eleos-Air.attlocal.net>
2021-12-13 07:01:50 -0800

    Pushing final app.py file and Readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sqlalchemy-challenge
 Repository for the SQL Alchemy challenge
+
+This project contains the SQL Alchemy challenge.
+
+In this repository you will find 1 resources folder, and 3 files (climate_JPYNB.jpynb, app.py and hawaii.sqlite).
+
+The Resources folder contains two (2) csv. files hawaii_measurements.csv and hawaii_stations.csv:
+hawaii_measuramentes.csv: Contains station (station that made measurement), date (date when it was taken), prcp (precipitation information) and tobs (temperature observation) data.
+hawaii_stations.csv:   Contains station, name, latitude, longitude and elevation data for each of the stations.
+
+These files are the resources used for the analysis and processes.
+
+The climate_JPYNB.jpynb file contains a pandas file with the pertinent analysis and charts obtained from the data.
+
+The app.py file is a python file containing an analysis using Flask.
--- app.py
+++ app.py
@@ -39,8 +39,9 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/<start_date><br/>"
+        f"/api/v1.0/<start_date>/<end_date><br/>"
+        f"Dates muste be entered in the following manner yyyy-mm-dd<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -89,16 +90,62 @@ def tobs():
     # Query stations
     results = session.query(Measurement.date, Measurement.tobs).\
               filter(Measurement.station == 'USC00519281').\
-                  filter(Measurement.date > '2016-08-23').all()   
+                  filter(Measurement.date >= '2016-08-23').filter(Measurement.date <= '2017-08-23').all()  
     session.close()
 
-    tobs = []
+    tobs_list = []
     for date, tobs in results:
         tobs_d = {}
-        tobs_d['Date'] = date
-        tobs_d['tobs'] = tobs
-        tobs.append(tobs_d)
-    return jsonify(tobs)
+        tobs_d["Date"] = date
+        tobs_d["Tobs"] = tobs
+        tobs_list.append(tobs_d)
+    return jsonify(tobs_list)
+
+
+
+@app.route("/api/v1.0/<start_date>")
+def start_date(start_date):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+    When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+    When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive."""
+
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+    session.close()
+
+    tobs_start = []
+    for min,max,avg in results:
+        start_dict = {}
+        start_dict["TMin"] = min
+        start_dict["TMax"] = max
+        start_dict["TAvg"] = avg
+        tobs_start.append(start_dict)
+    return jsonify(tobs_start)
+
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def start_end(start_date, end_date):
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).\
+            filter(Measurement.date <= end_date).all()
+    session.close()
+
+    start_end= []
+    for min,max,avg in results:
+        s_e_dict = {}
+        s_e_dict["TMin"] = min
+        s_e_dict["TMax"] = max
+        s_e_dict["TAvg"] = avg
+        start_end.append(s_e_dict)
+    return jsonify(start_end)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
